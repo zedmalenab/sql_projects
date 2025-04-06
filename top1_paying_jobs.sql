b76d3de98e13fdd93c682a278ba74e10fcac472b
Zed Malenab <zeddzed5@gmail.com>
2025-04-06 23:42:29 +0800

    prj1

---
+++ top1_paying_jobs.sql
@@ -0,0 +1,25 @@
+-- TOP 10 HIGHEST SALARY REMOTE JOB FOR A DATA ANALYST ROLE --
+
+
+SELECT
+    job_id,
+    job_title,
+    company_dim.name,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact
+    LEFT JOIN company_dim
+    ON job_postings_fact.company_id = company_dim.company_id
+
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+
+ORDER BY 
+    salary_year_avg DESC
+
+LIMIT 10
