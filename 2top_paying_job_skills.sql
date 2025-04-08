c14755773c2ef1608843e944108d79204692db9b
Zed Malenab <zeddzed5@gmail.com>
2025-04-08 20:10:50 +0800

    changes

---
+++ 2top_paying_job_skills.sql
@@ -0,0 +1,45 @@
+
+-- identifying top paying job skills from the previous table using CTE --
+WITH top_paying_jobs AS (
+SELECT
+    job_id,
+    job_title,
+    company_dim.name,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM
+    job_postings_fact
+    LEFT JOIN company_dim
+    ON job_postings_fact.company_id = company_dim.company_id
+
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+
+ORDER BY 
+    salary_year_avg DESC
+
+)
+
+SELECT
+    top_paying_jobs.*,
+    skills_dim.skills
+FROM top_paying_jobs
+INNER JOIN skills_job_dim AS skill_to_job
+    ON top_paying_jobs.job_id = skill_to_job.job_id
+INNER JOIN skills_dim
+    ON skill_to_job.skill_id = skills_dim.skill_id
+
+ORDER BY 
+    salary_year_avg DESC
+
+/* 
+Here's the breakdown of the most demanded skills for data analyst in 2023 based on job postings:
+SQL is leading with a bold count of 8.
+Python follows closely with a bold count of 7.
+Tableu is also a highly sought after, with a bold count of 6.
+Other skills like R, Snowflake, Pandas, and Excel show varying degrees of demand
+*/
