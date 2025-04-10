ce8f6fceb4ad0a0e1660694cf75230831e5fbbc3
Zed Malenab <zeddzed5@gmail.com>
2025-04-11 00:50:12 +0800

    changes

--- 4top_paying_skills.sql
+++ 4top_paying_skills.sql
@@ -10,23 +10,25 @@ Question: What are the top skills based on salary?
 
 -- Answer Query--
 
-SELECT 
+SELECT
     skills_dim.skills,
-    ROUND(AVG(job_postings_fact.salary_year_avg), 0) AS avg_salary
+    ROUND(AVG(salary_year_avg), 0) AS avg_salary
 
-FROM job_postings_fact
-INNER JOIN skills_job_dim AS skill_to_job
-    ON job_postings_fact.job_id = skill_to_job.job_id
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim
+    ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim
-    ON skill_to_job.skill_id = skills_dim.skill_id
+    ON skills_job_dim.skill_id = skills_dim.skill_id
 
 WHERE
-    job_title_short = 'Data Analyst' AND
-    salary_year_avg IS NOT NULL AND
-    job_work_from_home = true
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL 
+    AND job_work_from_home = true
 
 GROUP BY
-    skills
+    skills_dim.skills
+
 ORDER BY
     avg_salary DESC
 
@@ -47,3 +49,17 @@ as analysts increasingly work in production environments.
 and cross-functional tools positions analysts for top-tier roles.
 
 */
+
+
+
+
+/*
+
+Question: What are the top skills based on salary?
+- Look at the average salary associated with each skill for Data Analyst
+- Focus on the roles specified salaries regardless of location
+- Why? It reveals different skills impact salary levels for Data Analyst and 
+  help identify the most financially rewarding skill
+*/
+
+
